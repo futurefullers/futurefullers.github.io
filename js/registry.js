0e5b03c734d3db1f94d1f6de84d57b2e730325f0
Patrick Fuller <patrickfuller@gmail.com>
2017-05-31 23:08:07 -0500

    Added registry page

---
+++ js/registry.js
@@ -0,0 +1,7 @@
+/* jshint globalstrict: true, undef: true, unused: true, esversion: 6 */
+/* global setTimeout, $ */
+'use strict';
+
+$(() => {
+    setTimeout(() => $('.registry-thanks').fadeIn(2000), 2000);
+});
